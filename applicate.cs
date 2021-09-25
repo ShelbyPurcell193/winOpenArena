f3536636f882e574fdebbd70be60eb4c7405454d
ShelbyPurcell193 <52152837+ShelbyPurcell193@users.noreply.github.com>
2021-09-25 01:12:17 -0500

    Create applicate.cs

---
+++ applicate.cs
@@ -0,0 +1,28 @@
+namespace windesktop{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Web;
+    using System.Xml;
+    public class Apply{
+        System.ConsoleKeyInfo input;
+        System.StringBuilder area;
+        
+        
+        
+        public Apply(){
+            CancelKeyPress+=ConsoleCancelKeyPress(CancelKeyPress);
+        }
+           
+        public static char Main(String inp){
+            
+            return('');
+        }
+        protected static void CancelKeyPress(){
+            
+        }
+        ~Apply(){
+        
+        }
+    }
+}
